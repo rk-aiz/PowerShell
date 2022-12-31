812fd5f96348d9b40154d1b603ae3ee3a0498609
rk-aiz <rkaiz@outlook.jp>
2023-01-01 04:21:58 +0900

    SyncIMEStatus.ps1 ・KeyEventCallback メソッド の処理条件をWM_KEYDOWNからWM_KEYUPに変更     →IMEコンポジジション未確定文字が確定されるのを待つことができる ・WinEventCallbackメソッドの処理条件に0x70000000 < eventTypeを追加     →マウス操作によるIME ON/OFF切り替えを無効化

--- SyncIMEStatus.ps1
+++ SyncIMEStatus.ps1
@@ -5,6 +5,7 @@
 
 $WinEventTypeDef = @'
 using System;
+using System.Text;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
@@ -12,8 +13,8 @@ using System.Windows.Forms;
 public class WinEvent {
 
     //IME ON/OFFを切り替えるキーのコード
-    // { [(int)キーコード], [0:IME OFF, 1:IME ON], [0: IME OFF時に未確定文字をクリア, 1:IME OFF時に未確定文字を確定] }
-    private static int[,] IMEStateKeyCode = { {(int)Keys.IMENonconvert, 0, 1}, {(int)Keys.IMEConvert, 1, 0} };
+    // { [(int)キーコード], [0:IME OFF, 1:IME ON] }
+    private static int[,] IMEStateKeyCode = { {(int)Keys.IMENonconvert, 0}, {(int)Keys.IMEConvert, 1} };
     private static string[] IMEStateMsg = { "IME OFF", "IME ON" };
 
     [DllImport("user32.dll")]
@@ -45,8 +46,9 @@ public class WinEvent {
 
     private const uint WINEVENT_OUTOFCONTEXT = 0x0000;
     private const uint EVENT_SYSTEM_FOREGROUND = 0x0003;
-    private const int WH_KEYBOARD_LL = 13;
-    private const int WM_KEYDOWN = 0x0100;
+    private const uint EVENT_MAX = 0x7FFFFFFF;
+    private const int WH_KEYBOARD_LL = 0x000D;
+    private const int WM_KEYUP = 0x0101;
     private const int WM_IME_CONTROL = 0x0283;
     private const int IMC_GETOPENSTATUS = 0x0005;
     private const int IMC_SETOPENSTATUS = 0x0006;
@@ -71,7 +73,8 @@ public class WinEvent {
             if (IntPtr.Zero == keyEventHookId) {
                 return false;
             }
-            winEventHookId = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, winEventHookProc, 0, 0, WINEVENT_OUTOFCONTEXT);
+
+            winEventHookId = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_MAX, IntPtr.Zero, winEventHookProc, 0, 0, WINEVENT_OUTOFCONTEXT);
             if (IntPtr.Zero == winEventHookId) {
                 return false;
             }
@@ -95,29 +98,33 @@ public class WinEvent {
 
     private static void WinEventCallback(IntPtr hWinEventHook, uint eventType, IntPtr hWnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
     {
-        IntPtr imeHwnd = ImmGetDefaultIMEWnd(hWnd);
-
-        if (SendMessage(imeHwnd, WM_IME_CONTROL, IMC_GETOPENSTATUS, 0) != stateIme) {
-            SendMessage(imeHwnd, WM_IME_CONTROL, IMC_SETOPENSTATUS, stateIme);
+        if (eventType == EVENT_SYSTEM_FOREGROUND || 0x70000000 < eventType) {
+            IntPtr imeHwnd = ImmGetDefaultIMEWnd(hWnd);
+            if (SendMessage(imeHwnd, WM_IME_CONTROL, IMC_GETOPENSTATUS, 0) != stateIme) {
+                SendMessage(imeHwnd, WM_IME_CONTROL, IMC_SETOPENSTATUS, stateIme);
+            }
         }
     }
 
     private static IntPtr KeyEventCallback(int nCode, IntPtr wParam, IntPtr lParam)
     {
-        if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+        IntPtr lResult = CallNextHookEx(keyEventHookId, nCode, wParam, lParam);
+        if (nCode >= 0 && wParam == (IntPtr)WM_KEYUP) {
             int keyCode = Marshal.ReadInt32(lParam);
             for (int i = 0; i < IMEStateKeyCode.GetLength(0); i++) {
                 if (IMEStateKeyCode[i, 0] == keyCode) {
                     if (IMEStateKeyCode[i, 1] != stateIme) {
                         stateIme = IMEStateKeyCode[i, 1];
-                        //SendMessage(ImmGetDefaultIMEWnd(GetForegroundWindow()), WM_IME_CONTROL, IMC_SETOPENSTATUS, stateIme);
                         Console.WriteLine(IMEStateMsg[stateIme]);
+                        IntPtr hWnd = GetForegroundWindow();
+                        IntPtr imeHwnd = ImmGetDefaultIMEWnd(hWnd);
+                        SendMessage(imeHwnd, WM_IME_CONTROL, IMC_SETOPENSTATUS, stateIme);
                     }
                     break;
                 }
             }
         }
-        return CallNextHookEx(keyEventHookId, nCode, wParam, lParam);
+        return lResult;
     }
 }
 '@
@@ -128,7 +135,7 @@ $Script = {
     Try {
         [void][WinEvent]
     } Catch {
-        Add-Type -TypeDefinition $Args[0] -ReferencedAssemblies System.Windows.Forms
+        Add-Type -TypeDefinition $Args[0] -ReferencedAssemblies System.Windows.Forms, System.Runtime
     }
 
     $appContext = New-Object Windows.Forms.ApplicationContext
