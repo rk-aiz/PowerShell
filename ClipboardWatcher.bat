3a8ef22228247d2146528e0b57d6b52c45fa9729
rk-aiz <rkaiz@outlook.jp>
2023-01-13 01:53:15 +0900

    スタートアップ起動用batファイル追加

--- ClipboardWatcher.bat
+++ ClipboardWatcher.bat
@@ -1,13 +1,5 @@
-﻿@echo off
+@echo off
 chcp 65001 > nul
-
-if "%~1"=="-?" (
-    echo 起動オプション
-    echo -AlwaysOnTop 最前面状態で起動
-    echo -ShowConsole コンソール表示 (デバッグ用)
-    echo -UserDebug デバッグメッセージ表示
-    exit /b
-)
 
 powershell.exe -ExecutionPolicy Unrestricted -File "%~dp0%~n0.ps1"
 
--- SyncIMEStatus.bat
+++ SyncIMEStatus.bat
@@ -1,12 +1,5 @@
-﻿@echo off
+@echo off
 chcp 65001 > nul
-
-if "%~1"=="-?" (
-    echo -IgnoreZenHan : 全角/半角キー無効
-    echo -Disable : 無効状態で起動
-    echo -ShowConsole : コンソール表示
-    exit /b
-)
 
 powershell.exe -ExecutionPolicy Unrestricted -File "%~dp0%~n0.ps1" -IgnoreZenHan
 
